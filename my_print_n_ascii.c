646dca96ca32ef31f98a14a95c23b38463e716d0
robingoullioud <167295087+robingoullioud@users.noreply.github.com>
2024-04-17 12:13:31 +0000

    add my_print_n_ascii.c

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -1,13 +1,16 @@
-
 void print_char(char c);
+void my_print_n_ascii(int howMany);
 
-if my_print_n_ascii(int howMany+33>0)
-    
 void my_print_n_ascii(int howMany)
 {
-    for(int a=33; a<127; a=a+1)
-    {
-        char c=a ;
-        print_char(c);
-    }
+    int a = 33;
+    if ( (a < howMany+33) &&(127 > howMany+33) )
+        for(int a=33; a<howMany+33; a=a+1)
+        {
+            char c=a ;
+            print_char(c);
+        }
+    
+        
 }
+
